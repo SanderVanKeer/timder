366557123839d330716d7fdaf69ea4dc28b4a362
Mathijsvdb <mathijsvandenbroeck@gmail.com>
2015-05-10 14:25:27 +0200

    added extra routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET signup page. */
+router.get('/', function(req, res, next) {
+  res.render('signup');
+});
+
+/* GET user profile page. */
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
+
 
 module.exports = router;

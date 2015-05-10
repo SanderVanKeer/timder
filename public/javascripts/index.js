8f7dd0432c6b96d812c1dc761117dde5b1fefb0c
SanderVanKeer <sacevankeer@hotmail.com>
2015-05-10 14:36:18 +0200

    added the javascript

---
+++ public/javascripts/index.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+	// connect to socket.io
+	var socket = io.connect('http://localhost:3000');
+
+    $('#startSwiping').on('click', function(e) {
+    	e.preventDefault();
+    	console.log('geklikt');
+
+    	var company = $('#companyName').val();
+
+    	console.log(company);
+
+    	socket.emit('test', company);
+    });
+});

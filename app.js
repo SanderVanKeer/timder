3c3a6647ed823c896a93c1c38da793357d8405e8
Mathijsvdb <mathijsvandenbroeck@gmail.com>
2015-05-10 15:41:15 +0200

    clean up of all conflicts

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-<<<<<<< HEAD
-=======
-
->>>>>>> views
 
 var app = express();
 
@@ -26,10 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-<<<<<<< HEAD
-=======
-
->>>>>>> views
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,77 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-var isAuthenticated = function (req, res, next) {
-	// if user is authenticated in the session, call the next() to call the next request handler 
-	// Passport adds this method to request object. A middleware is allowed to add properties to
-	// request and response objects
-	if (req.isAuthenticated())
-		return next();
-	// if the user is not authenticated then redirect him to the login page
-	res.redirect('/');
-}
+/* GET landing page. */
+router.get('/', function(req, res){
+	res.render('index');
+});
 
-module.exports = function(passport){
-	/* GET landing page. */
-	router.get('/', function(req, res){
-		// Display the landing page.
-		res.render('index');
-	});
+/* GET login page. */
+router.get('/login', function(req, res) {
+	res.render('login');
+});
 
-	/* GET login page. */
-	router.get('/login', function(req, res) {
-    	// Display the Login page with any flash message, if any
-		res.render('login', { message: req.flash('message') });
-	});
-
-	//swiping page
-
-	router.get('/swiping', function(req, res) {
-    	// Display the Login page with any flash message, if any
-		res.render('swiping', { message: req.flash('message') });
-	});
-
-	/* Handle Login POST */
-	router.post('/login', passport.authenticate('login', {
-		successRedirect: '/student',
-		failureRedirect: '/login',
-		failureFlash : true  
-	}));
-
-	/* GET Registration Page */
-	router.get('/signup', function(req, res){
-		res.render('register',{message: req.flash('message')});
-	});
-
-	/* Handle Registration POST */
-	router.post('/signup', passport.authenticate('signup', {
-		successRedirect: '/home',
-		failureRedirect: '/signup',
-		failureFlash : true
-	}));
-
-	/* GET Home Page */
-	router.get('/home', isAuthenticated, function(req, res){
-		res.render('home', { user: req.user });
-	});
-
-	/* Handle Logout */
-	router.get('/signout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
-
-	return router;
-}
-
-
-<<<<<<< HEAD
 /* GET signup page. */
-router.get('/signup', function(req, res, next) {
+router.get('/signup', function(req, res) {
   res.render('signup');
 });
-=======
->>>>>>> views
+
+/* GET swiping page. */
+router.get('/swiping', function(req, res) {
+	res.render('swiping');
+});
+
+module.exports = router;
 
 
 

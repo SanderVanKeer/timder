159796741426262414ded04e0afb51d8becdb610
Mathijsvdb <mathijsvandenbroeck@gmail.com>
2015-05-12 15:09:55 +0200

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,64 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
 
-/* GET landing page. */
-router.route('/')  
-  .get(function(req, res, next) {
-    res.render('index');
-  })
-  .post(function(req, res, next) {
-    req.session.companyName = req.body.companyName;
-    res.redirect('letsdate');
+module.exports = function(passport){
+  /* GET landing page. */
+  router.route('/')  
+    .get(function(req, res, next) {
+      res.render('index');
+    })
+    .post(function(req, res, next) {
+      req.session.companyName = req.body.companyName;
+      res.redirect('letsdate');
+    });
+
+  router.post('/', function(req, res) {
+    console.log('test');
   });
 
-router.post('/', function(req, res) {
-  console.log('test');
-});
+  /* GET login page. */
+  router.get('/login', function(req, res) {
+    res.render('login', {message: req.flash('message')});
+  });
 
-/* GET login page. */
-router.get('/login', function(req, res) {
-	res.render('login', {message: req.flash('message')});
-});
+  /* POST login page */
+  router.post('/login', passport.authenticate('login', {
+    successRedirect: '/addwork',
+    failureRedirect: '/login',
+    failureFlash : true  
+  }));
 
-/* POST login page */
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/addwork',
-  failureRedirect: '/login',
-  failureFlash : true  
-}));
+  /* GET signup page. */
+  router.get('/signup', function(req, res) {
+    res.render('signup');
+  });
 
-/* GET signup page. */
-router.get('/signup', function(req, res) {
-  res.render('signup');
-});
+  /* POST signup page. */
+  router.post('/signup', passport.authenticate('signup', {
+    successRedirect: '/addwork',
+    failureRedirect: '/signup',
+    failureFlash : true  
+  }));
 
-/* GET letsdate page. */
-router.get('/letsdate', function(req, res) {
-  var companyName = req.session.companyName;
-	res.render('letsdate', {companyName: companyName});
-});
+  /* GET letsdate page. */
+  router.get('/letsdate', function(req, res) {
+    var companyName = req.session.companyName;
+    res.render('letsdate', {companyName: companyName});
+  });
 
-/* GET addwork page */
-router.get('/addwork', function(req, res, next) {
-  res.render('addwork', {user: req.user})
-});
+  /* GET addwork page */
+  router.get('/addwork', function(req, res, next) {
+    res.render('addwork', {user: req.user})
+  });
 
-/* Handle Logout */
-router.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
-});
+  /* Handle Logout */
+  router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 
-module.exports = router;
+  return router;
+}
 
 
 

170bb6800d5a86c2c14c4d9d979741e4f22f7a8e
Mathijsvdb <mathijsvandenbroeck@gmail.com>
2015-05-12 03:46:43 +0200

    added login POST + clean up

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET landing page. */
 router.route('/')  
-    .get(function(req, res, next) {
-      res.render('index');
-    })
-    .post(function(req, res, next) {
-      req.session.companyName = req.body.companyName;
-      res.redirect('letsdate');
-    });
+  .get(function(req, res, next) {
+    res.render('index');
+  })
+  .post(function(req, res, next) {
+    req.session.companyName = req.body.companyName;
+    res.redirect('letsdate');
+  });
 
 router.post('/', function(req, res) {
   console.log('test');
 });
 
-/* GET login page. */
-router.get('/login', function(req, res) {
-	res.render('login');
-});
+// /* GET login page. */
+// router.get('/login', function(req, res) {
+// 	res.render('login');
+// });
+
+/* login page */
+router.route('/login')
+  .get(function(req, res, next) {
+    res.render('login');
+  })
+  .post(passport.authenticate('local'), function(req, res) {
+    res.send('testing passport post');
+  })
 
 /* GET signup page. */
 router.get('/signup', function(req, res) {
